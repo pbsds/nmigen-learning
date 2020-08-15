b38296538f74d657c939ed5e06cdce551f29220e
Peder Bergebakken Sundt <pbsds@hotmail.com>
2020-08-15 15:48:17 +0200

    Implement a simple VGA controller

--- fpga/resources/pmod.py
+++ fpga/resources/pmod.py
@@ -4,9 +4,7 @@ from functools import partial
 __all__ = []
 def pmod(func):
     __all__.append(func.__name__)
-    return partial(func,
-        name = func.__name__,
-    )
+    return partial(func, name=func.__name__)
 
 
 # Icebreaker PMODs
@@ -14,7 +12,7 @@ def pmod(func):
 #subsignal_args = [Attrs(IO_STANDARD="SB_LVCMOS33")]
 
 @pmod
-def seven_seg(number, *, pmod, name = __name__, subsignal_args=(), extras={}):
+def seven_seg(number, *, pmod, name=__name__, subsignal_args=(), extras={}):
     return [Resource(name, number,
          Subsignal("aa", PinsN( "1", dir="o", conn=("pmod", pmod)), *subsignal_args),
          Subsignal("ab", PinsN( "2", dir="o", conn=("pmod", pmod)), *subsignal_args),
@@ -29,7 +27,7 @@ def seven_seg(number, *, pmod, name = __name__, subsignal_args=(), extras={}):
 
 
 @pmod
-def dip_switch8(number, *, pmod, name = __name__, subsignal_args=(), extras={}):
+def dip_switch8(number, *, pmod, name=__name__, subsignal_args=(), extras={}):
     return [Resource(name, number,
          Subsignal("d1", PinsN( "1", dir="i", conn=("pmod", pmod)), *subsignal_args),
          Subsignal("d2", PinsN( "2", dir="i", conn=("pmod", pmod)), *subsignal_args),
@@ -41,3 +39,32 @@ def dip_switch8(number, *, pmod, name = __name__, subsignal_args=(), extras={}):
          Subsignal("d8", PinsN("10", dir="i", conn=("pmod", pmod)), *subsignal_args),
          **extras,
      )]
+
+
+@pmod
+def dvi_3bit(number, *, pmod1=0, pmod2=1, name=__name__, subsignal_args=(), extras={}):
+    return [Resource(name, number,
+        Subsignal("r",  Pins (" 1", dir="o", conn=("pmod", pmod1)), *subsignal_args), # red
+        Subsignal("g",  Pins (" 2", dir="o", conn=("pmod", pmod1)), *subsignal_args), # green
+        Subsignal("b",  Pins (" 3", dir="o", conn=("pmod", pmod2)), *subsignal_args), # blue
+        Subsignal("ck", Pins (" 4", dir="o", conn=("pmod", pmod2)), *subsignal_args), # data clock
+        Subsignal("de", Pins (" 7", dir="o", conn=("pmod", pmod2)), *subsignal_args), # data enable
+        Subsignal("hs", Pins (" 8", dir="o", conn=("pmod", pmod2)), *subsignal_args), # hsync
+        Subsignal("vs", Pins (" 9", dir="o", conn=("pmod", pmod2)), *subsignal_args), # vsync
+        #Subsignal("", Pins ("10", dir="o", conn=("pmod", pmod2)), *subsignal_args),
+        **extras,
+    )]
+
+
+@pmod
+def dvi_12bit(number, *, pmod1=0, pmod2=1, name=__name__, subsignal_args=(), extras={}):
+    return [Resource(name, number,
+        Subsignal("r",  Pins (" 8 7 2 1", dir="o", conn=("pmod", pmod1)), *subsignal_args), # red
+        Subsignal("g",  Pins ("10 9 4 3", dir="o", conn=("pmod", pmod1)), *subsignal_args), # green
+        Subsignal("b",  Pins (" 3 7 8 1", dir="o", conn=("pmod", pmod2)), *subsignal_args), # blue
+        Subsignal("ck", Pins (" 2", dir="o", conn=("pmod", pmod2)), *subsignal_args),       # data clock
+        Subsignal("de", Pins (" 9", dir="o", conn=("pmod", pmod2)), *subsignal_args),       # data enable
+        Subsignal("hs", PinsN(" 4", dir="o", conn=("pmod", pmod2)), *subsignal_args),       # hsync
+        Subsignal("vs", Pins ("10", dir="o", conn=("pmod", pmod2)), *subsignal_args),       # vsync
+        **extras,
+    )]
